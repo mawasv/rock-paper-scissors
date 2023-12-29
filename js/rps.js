ef84a5c2fd2532a71df0da4c04b8de8512394a70
mawasv <mawasv@gmail.com>
2023-12-29 16:52:07 +0100

    Get a round of Rock, Paper, Scissors going!

--- js/rps.js
+++ js/rps.js
@@ -1,9 +1,36 @@
 function getComputerChoice(computerChoice) {
-    const arr = ["Rock", "Paper", "Scissors"];
+    const rockPaperScissors = ["rock", "paper", "scissors"];
     function random(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
-    } computerChoice = arr[random(0, 2)];
+    } computerChoice = rockPaperScissors[random(0, 2)];
     return computerChoice;
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    const rock = "rock";
+    const paper = "paper";
+    const scissors = "scissors";
+    if (playerSelection === rock.toLowerCase() && computerSelection === scissors.toLowerCase()) {
+        return "You win! Rock beats scissors.";
+    } else if (playerSelection === rock.toLowerCase() && computerSelection === paper.toLowerCase()) {
+        return "You lose! Paper beats rock.";
+    } else if (playerSelection === rock.toLowerCase() && computerSelection === rock.toLowerCase()) {
+        return "Draw!";
+    } else if (playerSelection === paper.toLowerCase() && computerSelection === scissors.toLowerCase()) {
+        return "You lose! Scissors beat paper.";
+    } else if (playerSelection === paper.toLowerCase() && computerSelection === paper.toLowerCase()) {
+        return "Draw!";
+    } else if (playerSelection === paper.toLowerCase() && computerSelection === rock.toLowerCase()) {
+        return "You win! Paper beats rock.";
+    } else if (playerSelection === scissors.toLowerCase() && computerSelection === scissors.toLowerCase()) {
+        return "Draw!";
+    } else if (playerSelection === scissors.toLowerCase() && computerSelection === paper.toLowerCase()) {
+        return "You win! Scissors beat paper.";
+    } else if (playerSelection === scissors.toLowerCase() && computerSelection === rock.toLowerCase()) {
+        return "You lose! Rock beats scissors.";
+    }
+  }
+   
+  const playerSelection = "rock";
+  const computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection, computerSelection));

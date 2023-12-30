09f6cd86fb0023d978d9de78566cc77102b17cfe
mawasv <mawasv@gmail.com>
2023-12-30 10:34:19 +0100

    Tidy up and get a somewhat working version of the game up and running.

--- js/rps.js
+++ js/rps.js
@@ -1,36 +1,75 @@
+// Computer's choice
 function getComputerChoice(computerChoice) {
+
     const rockPaperScissors = ["rock", "paper", "scissors"];
+
     function random(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     } computerChoice = rockPaperScissors[random(0, 2)];
+
     return computerChoice;
 }
 
+// Get player's choice
+
+function getPlayerSelection() {
+    let playerInput = prompt("Choose your weapon!");
+    let result = playerInput.toLowerCase();
+    return result;
+} 
+
+// Play one round and save the score
 function playRound(playerSelection, computerSelection) {
-    const rock = "rock";
-    const paper = "paper";
-    const scissors = "scissors";
-    if (playerSelection === rock.toLowerCase() && computerSelection === scissors.toLowerCase()) {
+
+    if (playerSelection === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats scissors.";
-    } else if (playerSelection === rock.toLowerCase() && computerSelection === paper.toLowerCase()) {
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
         return "You lose! Paper beats rock.";
-    } else if (playerSelection === rock.toLowerCase() && computerSelection === rock.toLowerCase()) {
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
         return "Draw!";
-    } else if (playerSelection === paper.toLowerCase() && computerSelection === scissors.toLowerCase()) {
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
         return "You lose! Scissors beat paper.";
-    } else if (playerSelection === paper.toLowerCase() && computerSelection === paper.toLowerCase()) {
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
         return "Draw!";
-    } else if (playerSelection === paper.toLowerCase() && computerSelection === rock.toLowerCase()) {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
         return "You win! Paper beats rock.";
-    } else if (playerSelection === scissors.toLowerCase() && computerSelection === scissors.toLowerCase()) {
+    } else if (playerSelection === "scissors" && computerSelection === "scissors") {
         return "Draw!";
-    } else if (playerSelection === scissors.toLowerCase() && computerSelection === paper.toLowerCase()) {
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
         return "You win! Scissors beat paper.";
-    } else if (playerSelection === scissors.toLowerCase() && computerSelection === rock.toLowerCase()) {
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
         return "You lose! Rock beats scissors.";
     }
-  }
-   
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+}
+
+// For triggering the rounds and keeping track of the score
+function game() {
+
+let = playerScore = 0
+let = computerScore = 0
+
+for (let i = 0; i < 5; i++) {
+    const result = playRound(getPlayerSelection(), getComputerChoice());
+    if (result.includes("win")) {
+        playerScore++;
+        console.log(`Player score: ${playerScore} | Computer score: ${computerScore}`);
+    } else if (result.includes("lose")) {
+        computerScore++;
+        console.log(`Player score: ${playerScore} | Computer score: ${computerScore}`);
+    }
+
+// Announcement of the final results
+console.log(`Final results: ${playerScore} | Computer score: ${computerScore}`);
+if (playerScore > computerScore) {
+    console.log("Congratulations! You win the game.");
+} else if (computerScore > playerScore) {
+    console.log("Too bad! You lost the game.");
+} else if (playerScore === computerScore) {
+    console.log("The game was an overall tie!");
+}
+
+}
+
+}
+
+game();
